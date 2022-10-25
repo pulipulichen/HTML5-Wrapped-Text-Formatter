ea5b14e374ac1d884a916c478d32366fbea9d1fa
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-10-25 21:55:49 +0800

    Tue Oct 25 09:55:49 PM CST 2022

--- service-worker.js
+++ service-worker.js
@@ -16,7 +16,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v2021-0221-20220920-1210'
+const PRECACHE = 'precache-v2021-0221-20220920-1211'
 const RUNTIME = 'runtime';
 
 /**
