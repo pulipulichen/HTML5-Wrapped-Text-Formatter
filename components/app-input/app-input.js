4a0839be261dbf7cdb24f4481cd552224e7bfa26
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-05-03 06:58:29 +0800

    Wed May  3 06:58:29 AM CST 2023

--- components/app-input/app-input.js
+++ components/app-input/app-input.js
@@ -99,6 +99,7 @@ let appInput = {
       result = this.formatJoinDash(result)
       result = this.formatJoinNewLine(result)
       result = this.formatRemoveQuote(result)
+      result = this.formatRemoveQuoteSquareBrackets(result)
       result = this.formatRemoveDoubleSpaces(result)
       result = this.formatConvertPunctuationMarks(result)
       // result = this.formatConvertQuotationToHalf(result)
@@ -401,6 +402,53 @@ let appInput = {
         splitor = ['（', '）']
       }
      
+      text.split(splitor[0]).forEach((t, i) => {
+        // console.log(t, i)
+        if (i === 0) {
+          output.push(t)
+          return true
+        }
+
+        let pos = t.lastIndexOf(splitor[1])
+        if (pos === -1) {
+          output.push(splitor[0] + t)
+          return true
+        }
+
+        // let partInBrackets = t.slice(0, pos)
+        let partOutBrackets = t.slice(pos + 1)
+
+        output.push(partOutBrackets)
+      })
+
+      text = output.join('')
+
+      while (text.indexOf('  ') > -1) {
+        text = text.replace(/  /g, ' ')
+      }
+
+      if (this.transFromLang.startsWith('zh')) {
+        text = this.removeSpaceInChinese(text)
+        // console.log(text)
+      }
+      
+
+      return text
+    },
+    formatRemoveQuoteSquareBrackets (text) {
+      if (!this.db.localConfig.removeQuote) {
+        return text
+      }
+
+      let output = []
+      let isHalf = true
+      let leftPos = text.indexOf('[')
+      if (leftPos === -1) {
+        return text
+      }
+
+      let splitor = ['[', ']']
+      
       text.split(splitor[0]).forEach((t, i) => {
         // console.log(t, i)
         if (i === 0) {
