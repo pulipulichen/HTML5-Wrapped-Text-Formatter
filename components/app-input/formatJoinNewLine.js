90686b5fb31b3361f6b55bfd92f78db7c0421c96
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-09-25 04:55:57 +0800

    西元2023年09月25日 (週一) 04時55分56秒 CST

--- components/app-input/formatJoinNewLine.js
+++ components/app-input/formatJoinNewLine.js
@@ -4,13 +4,28 @@ let formatJoinNewLine = function (text) {
     return text
   }
 
-  // 解決中文換行問題
-  text = text.replace(/\n[\u4e00-\u9fa5]/g, function (_word) {
-    // alert(_word);
-    return _word.split('\n').join('')
-  })
+  while (text.indexOf(`\n\n\n`) > -1) {
+    text = text.split('\n\n\n').join('\n\n')
+  }
 
-  text = text.split('\n').join(' ')
+  text = text.split('\n\n').map(t => {
+    // console.log(t)
+
+    // 解決中文換行問題
+    t = t.replace(/\n[\u4e00-\u9fa5]/g, function (_word) {
+      // alert(_word);
+      return _word.split('\n').join('')
+    })
+
+    t = t.split('\n').join(' ')
+
+    return t
+  }).join('\n\n')
+
+  // while (text.indexOf(`\n\n\n`) > -1) {
+  //   text = text.split('\n\n\n').join('\n\n')
+  // }
+  // text = text.split('\n').join(' ')
 
   return text
 }
